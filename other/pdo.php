3b5332b884047d22da1e5f0c39436bb02fd3f6a6
a11dc908 <asia@hodo-no-MacBook-Pro.local>
2014-01-29 23:44:43 +0900

    a11dc908

--- other/pdo.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=webpro2exam;charset=utf8;', 'root', '');
-
-   }    
-   catch (PDOException $e) {
-    var_dump($e->getMessage());
-}
